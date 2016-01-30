37df11cea365c51ea6c05e8cce2bf3fe85f8710d
Herman Slatman <hermanslatman@hotmail.com>
2016-01-30 19:13:25 +0100

    Update for the reporting screen: introduce options button

---
+++ ForwardToAbuseAddin/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
